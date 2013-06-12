99e570bc1f940658f575b6327d6a3105b98a4875
Ekramvis <ekramvis@gmail.com>
2013-06-12 13:58:07 -0400

    visits log correctly. only logs at most two vists per day for any profile you view. does not log visits to your own profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController 
 	before_filter :authenticate_user!
+
+	# log_visit requires "id" in the params to correspond to a user's id
 	after_filter :log_visit
 	skip_after_filter :log_visit, only: [:new, :create, :index, :destroy, :edit, :update]
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,20 @@
 module ApplicationHelper
 
 	def log_visit
-		return nil if params[:user_id] == current_user.id 
-		 
+		#get rid of self-visits first
+		return nil if params[:id].to_i == current_user.id
+
+		#create a new visit event if one hasn't occurered in the last day
+		last_visit = Visit.where(visitor_id: current_user.id, visitee_id: params[:id]).order('id DESC')[0]
+		if last_visit
+			if Time.now - last_visit.created_at > 43200
+				Visit.create!(visitor_id: current_user.id, visitee_id: params[:id])
+			end
+		else
+			Visit.create!(visitor_id: current_user.id, visitee_id: params[:id])
+		end
+
+		#return nil, purely a side effect method
+		nil
 	end
 end
