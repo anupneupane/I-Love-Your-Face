06f313e87f83cbd8a0986759458bda588cb408e1
Ekramvis <ekramvis@gmail.com>
2013-06-12 12:08:36 -0400

    shell of log visit method working, need to complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController 
 	before_filter :authenticate_user!
+	after_filter :log_visit
+	skip_after_filter :log_visit, only: [:new, :create, :index, :destroy, :edit, :update]
+
 	
 	def show
 		@user = current_user 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
 
+	def log_visit
+		return nil if params[:user_id] == current_user.id 
+		 
+	end
 end
